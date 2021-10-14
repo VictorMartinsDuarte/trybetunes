cd1d00a2c069b5e0b9e46b25d77bc9b0deaaf01b
Victor Martins <victor.martins.duarte@gmail.com>
2021-10-14 09:10:57 -0300

    Testing Req 06 evaluator

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import MusicList from '../components/MusicList';
 
 class Album extends Component {
   render() {
+    console.log(this.props)
+    const { match: { params } } = this.props;
     return (
       <>
         <Header />
         <main data-testid="page-album">
-          Album
+          {/* <MusicList musicId={ params.id } /> */}
+          Texto
         </main>
       </>
     );
