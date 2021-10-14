913ebaa05c13d72de6740cb018d02860bf4acda7
Victor Martins <victor.martins.duarte@gmail.com>
2021-10-14 11:36:29 -0300

    Testing evaluator

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Profile extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+    };
+  }
+
+
   render() {
     return (
       <>
