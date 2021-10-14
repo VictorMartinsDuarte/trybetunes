9b40d319c1a5062a435ee263094144f5d523eb82
Victor Martins <victor.martins.duarte@gmail.com>
2021-10-14 11:52:06 -0300

    Requirement 11

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Profile extends Component {
   constructor() {
     super();
 
     this.state = {
-      loading: false,
+      loading: true,
+      name: '',
+      email: '',
+      image: '',
+      description: '',
     };
   }
 
+  componentDidMount() {
+    this.handleGetUserInfo();
+  }
+
+  handleGetUserInfo = async () => {
+    const getUserInfo = await getUser();
+    const { name, email, image, description } = getUserInfo;
+    this.setState({
+      loading: false,
+      name,
+      email,
+      image,
+      description,
+    });
+  }
 
   render() {
+    const { loading, name, email, image, description } = this.state;
+    const renderUserInfo = (
+      <main data-testid="page-profile">
+        <p>{ name }</p>
+        <p>{ email }</p>
+        <p>{ description }</p>
+        <img data-testid="profile-image" src={ image } alt="" />
+        <Link to="/profile/edit">Editar perfil</Link>
+      </main>
+    );
     return (
       <>
         <Header />
-        <main data-testid="page-profile">
-          Profile
-        </main>
+        {
+          loading
+            ? <Loading />
+            : renderUserInfo
+        }
       </>
     );
   }
