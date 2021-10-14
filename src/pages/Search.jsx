b8c2f535e7e6062cb120d3f643a6e7c2def719cc
Victor Martins <victor.martins.duarte@gmail.com>
2021-10-13 21:48:57 -0300

    Requirement 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,12 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      musicName: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({ [target.name]: target.value });
+  }
+
   render() {
+    const { musicName } = this.state;
+    const minNameLength = 2;
     return (
       <>
         <Header />
         <main data-testid="page-search">
-          Search
+          <form action="">
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ this.handleChange }
+              value={ musicName }
+              name="musicName"
+            />
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              disabled={ musicName.length < minNameLength }
+            >
+              Pesquisar
+
+            </button>
+          </form>
         </main>
       </>
     );
